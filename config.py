4aeb1eea62ae7ca6da558d2af3ebcff034399d8c
TeamAnsh <130488642+TeamAnsh@users.noreply.github.com>
2023-05-19 20:58:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", " https://te.legra.ph/file/04218b88e253a14220dc3.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/56d1760224589ee370186.jpg",
+    " https://te.legra.ph/file/04218b88e253a14220dc3.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
