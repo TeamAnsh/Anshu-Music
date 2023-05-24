e3225d36b9d8d6fbc5e0681837f7ca9c999df3a5
TeamAnsh <130488642+TeamAnsh@users.noreply.github.com>
2023-05-24 15:02:22 +0530

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,7 +12,7 @@ class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "𝙰𝚗𝚜𝚑𝚞❧𝙼𝚞𝚜𝚒𝚌",
+            "AnonXMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
